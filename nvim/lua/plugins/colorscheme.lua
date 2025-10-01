755f90d898dd36a6dfd500fc9d384e8e58d07e43
chenli9627 <chenli9627@gmail.com>
2025-10-01 21:52:11 +0800

    use nvim theme rose-pine

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -15,13 +15,28 @@
 -- }
 
 return {
-  "sainnhe/gruvbox-material",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    -- Optionally configure and load the colorscheme
-    -- directly inside the plugin declaration.
-    vim.g.gruvbox_material_enable_italic = true
-    vim.cmd.colorscheme("gruvbox-material")
-  end,
+  {
+    "sainnhe/gruvbox-material",
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --   -- Optionally configure and load the colorscheme
+    --   -- directly inside the plugin declaration.
+    --   vim.g.gruvbox_material_enable_italic = true
+    --   vim.cmd.colorscheme("gruvbox-material")
+    -- end,
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    -- config = function()
+    --   vim.cmd.colorscheme("nightfox")
+    -- end,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
 }
